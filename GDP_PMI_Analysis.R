855a475e292287a5750cb1aad25b5d6a14562d71
marco27183 <marco27183@gmail.ccom>
2021-10-23 22:44:58 +0200

    created time series object

--- GDP_PMI_Analysis.R
+++ GDP_PMI_Analysis.R
@@ -1,1 +1,7 @@
-pmi_gdp_raw = read.csv("./data/mydata.csv")
+pmi_gdp_raw = read.csv("./data/TS_Analysis_PMI_GDP.csv", row.names = 1)
+
+pmi_gdp_raw[,1] = pmi_gdp_raw[,1]*100
+
+pmi_gdp_ts = ts(pmi_gdp_raw, start=c(1947,4,1), frequency=12)
+
+plot(stl(pmi_gdp_ts[,1:9], s.window=12, na.action=na.omit))
